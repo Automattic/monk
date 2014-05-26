a89877066f6d3409d0e59d48d0378da2a9d62864
Adam Rudd <adam.rudd@uqconnect.edu.au>
2014-05-26 12:02:01 +0200

    Cast oids inside of $nor queries

--- test/collection.test.js
+++ test/collection.test.js
@@ -34,12 +34,21 @@ describe('collection', function () {
   });
 
   describe('cast', function () {
-    it('should cast ids inside $and', function () {
+    it('should cast oids inside $and', function () {
       var cast = users.cast({
         $and: [{_id: '4ee0fd75d6bd52107c000118'}]
       });
 
       var oid = users.id(cast.$and[0]._id);
+      expect(oid.toHexString()).to.equal('4ee0fd75d6bd52107c000118');
+    });
+
+    it('should cast oids inside $nor', function () {
+      var cast = users.cast({
+        $nor: [{_id: '4ee0fd75d6bd52107c000118'}]
+      });
+
+      var oid = users.id(cast.$nor[0]._id);
       expect(oid.toHexString()).to.equal('4ee0fd75d6bd52107c000118');
     });
 
